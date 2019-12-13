2f0274c6253dcf89e2bd03819e3aabddcdd9f030
Watson <watson1978@gmail.com>
2019-12-13 17:54:57 +0900

    update script for travis

--- travis/launch.sh
+++ travis/launch.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# https://qiita.com/TsutomuNakamura/items/676314035169b9ed2cd6
-IMAGE="travisci/ci-garnet:packer-1512502276-986baf0"
-docker run --name travis -dit $IMAGE /sbin/init
-docker exec -it travis bash -c "su travis"
+IMAGE="travisci/ubuntu-ruby:18.04"
+docker run --name travis -dit $IMAGE
+docker exec -it travis bash
